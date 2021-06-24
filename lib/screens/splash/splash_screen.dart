936fd9c90023fab9bf14cd205bffdea2ffda39d4
jinseo-lee <jinseo.lee@navercorp.com>
2021-06-24 23:05:51 +0900

    2021.06.24

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/screens/components/body.dart';
+
+import '../../size_config.dart';
 
 class SplashScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    // Add in start screen
+    SizeConfig().init(context);
+    return Scaffold(
+      body: Body(),
+    );
   }
 }
