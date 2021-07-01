5cfcb8dc1396b76aa627c8b6130c17907362c4a7
jinseo-lee <jinseo.lee@navercorp.com>
2021-07-01 22:40:31 +0900

    2021.07.01

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../size_config.dart';
+import 'categories.dart';
+import 'discount_banner.dart';
+import 'home_header.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: getProportionateScreenWidth(20),
+            ),
+            HomeHeader(),
+            SizedBox(
+              height: getProportionateScreenWidth(30),
+            ),
+            DiscountBanner(),
+            SizedBox(
+              height: getProportionateScreenWidth(30),
+            ),
+            Categories(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/screens/home/components/search_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+import '../../../size_config.dart';
+
+class SearchField extends StatelessWidget {
+  const SearchField({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: SizeConfig.screenWidth * 0.6,
+      //height: 50,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: kSecondaryColor.withOpacity(0.1),
+      ),
+      child: TextField(
+        onChanged: (value) {},
+        decoration: InputDecoration(
+            enabledBorder: InputBorder.none,
+            focusedBorder: InputBorder.none,
+            hintText: "Search Product",
+            prefixIcon: Icon(Icons.search),
+            contentPadding: EdgeInsets.symmetric(
+              horizontal: getProportionateScreenWidth(20),
+              vertical: getProportionateScreenHeight(9),
+            )),
+      ),
+    );
+  }
+}
